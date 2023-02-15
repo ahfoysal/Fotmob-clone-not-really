977a3153dc50c4584aeeafc51e030524988c8a2f
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-02-15 18:34:15 +0600

    sad
    sad

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -462,6 +462,7 @@ span{
                   
                     
             </div>
+            { details?.content?.lineup?.lineup &&
             
            <section className="css-7g2v2r-LineupMapContainer ">
            <div className="css-16vghmb-GoalContainer eu5dgts1"><svg xmlns="http://www.w3.org/2000/svg" id="lineup_1_214x118" width="276" height="154" viewBox="0 0 316 174"><g id="Group_4486" fill="var(--MFFullscreenColorScheme-pitchLines)" data-name="Group 4486" transform="translate(84.168)"><path id="Path_2174" d="M57 0h5.907v50.136a5.92 5.92 0 0 0 5.907 5.9H192.85a5.92 5.92 0 0 0 5.907-5.9V0h5.907v50.136a11.84 11.84 0 0 1-11.813 11.8H68.813A11.84 11.84 0 0 1 57 50.136z" className="cls-1" data-name="Path 2174" transform="translate(-57)"></path></g><path id="Path_2175" fill="var(--MFFullscreenColorScheme-pitchLines)" d="M11.813 150.407h90.813a76.778 76.778 0 0 0 110.748 0h90.813A11.839 11.839 0 0 0 316 138.61V0h-5.906v138.61a5.92 5.92 0 0 1-5.907 5.9H11.813a5.92 5.92 0 0 1-5.907-5.9V0H0v138.61a11.84 11.84 0 0 0 11.813 11.797zm193 0a70.761 70.761 0 0 1-93.619 0z" className="cls-1" data-name="Path 2175"></path></svg></div>
@@ -503,7 +504,7 @@ span{
            
            
            
-           </section> 
+           </section> }
 
     </Wrapper>
   )
